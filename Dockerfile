6228eec36e64d84ef13c45e6e78f43d91da65355
Eamon Bauman <eamon@eamonbauman.com>
2019-06-17 17:40:24 -0500

    build scripts and stuff

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM nginx
+
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+COPY dist/* /usr/share/nginx/html
---
+++ build.sh
@@ -0,0 +1,17 @@
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[", ]//g')
+
+echo "Building HobbyFarm UI $PACKAGE_VERSION"
+
+ng build --aot
+
+docker build -t "hobbyfarm/ui" . 
+
+docker tag "hobbyfarm/ui" "hobbyfarm/ui:$PACKAGE_VERSION"
+
+docker push "hobbyfarm/ui:$PACKAGE_VERSION"
+
+echo "HobbyFarm UI built. Pushed version $PACKAGE_VERSION to Docker hub"
