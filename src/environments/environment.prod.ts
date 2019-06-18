6d5d224eb9896fee78bd076ae7d477731e92b335
Eamon Bauman <eamon@eamonbauman.com>
2019-06-17 21:22:55 -0500

    i am not a clever man

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  serverhostname: "api.na1.hobbyfarm.io", // hostname only e.g. server.example.com
+  server: "https://api.na1.hobbyfarm.io", // full url e.g. https://server.example.com
 };
