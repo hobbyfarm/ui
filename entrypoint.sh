e5f17be0dfc0cb414777c774ab50fae2aedd4870
Eamon Bauman <eamon@eamonbauman.com>
2019-09-17 18:39:35 -0500

    changed bash expansion for new angular

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed -i "s/#####HF_SERVER#####/$HF_SERVER/g" /usr/share/nginx/html/main.*.js
+sed -i "s/#####HF_SERVER#####/$HF_SERVER/g" /usr/share/nginx/html/main*.*.js
 echo "Configured with HF_SERVER=$HF_SERVER"
 nginx -g 'daemon off;' # overriding nginx default startup
