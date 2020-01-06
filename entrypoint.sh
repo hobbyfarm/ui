465b50e13382b390a5fa0403560a5001ba859d56
Bryton Hall <email@bryton.io>
2020-01-06 12:02:35 -0500

    switch entrypoint to sh instead of bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 sed -i "s/#####HF_SERVER#####/$HF_SERVER/g" /usr/share/nginx/html/main*.*.js
 echo "Configured with HF_SERVER=$HF_SERVER"
 nginx -g 'daemon off;' # overriding nginx default startup
+
