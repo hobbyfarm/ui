8ef39a92c48ad410aad25bf0bd964ba6137fd7ff
Bryton Hall <email@bryton.io>
2020-01-24 22:42:28 -0500

    add missing buttons

--- src/app/services/step.service.ts
+++ src/app/services/step.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Step } from '../step/Step';
-import { Step } from '../Step';
 import { HttpClient } from '@angular/common/http';
 import { map, tap } from 'rxjs/operators';
 import { ServerResponse } from '../ServerResponse';
