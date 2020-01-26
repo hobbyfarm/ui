11ef46037990fb5d1b159e994dede84b7f30bad1
Bryton Hall <email@bryton.io>
2020-01-26 16:19:46 -0500

    update build and entrypoint scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # check all args for the insecure flag
-if [[ "$@" == *"--insecure"* ]]; then
-    HF_SERVER=http://$HF_SERVER
-else
+if [ "${*#*--insecure}" = "$*" ]; then
     HF_SERVER=https://$HF_SERVER
+else
+    HF_SERVER=http://$HF_SERVER
 fi
 
 sed -i "s|#####HF_SERVER#####|$HF_SERVER|g;" /usr/share/nginx/html/main*.*.js
