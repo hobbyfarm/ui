d47937301ebf7b748abdb6b4d5c0de3fbc99e1f4
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2021-09-21 17:40:20 +0200

    Add Dichromatic Theme, Better Styling

---
+++ src/app/scenario/terminal-themes/Dichromatic.ts
@@ -0,0 +1,29 @@
+export default {
+    background: '#000000',
+    foreground: '#ffffff',
+    cursor: '#ffffff',
+
+    black: '#2e3436',
+    brightBlack: '#555753',
+
+    red: '#cc0000',
+    brightRed: '#ef2929',
+
+    green: '#008700',
+    brightGreen: '#5FAF87',
+
+    yellow: '#AFAF00',
+    brightYellow: '#DFDF87',
+
+    blue: '#00005F',
+    brightBlue: '#729fcf',
+
+    magenta: '#87005F',
+    brightMagenta: '#ad7fa8',
+
+    cyan: '#87D7D7',
+    brightCyan: '#34e2e2',
+
+    white: '#d3d7cf',
+    brightWhite: '#FFFFFF'
+}
--- src/app/scenario/terminal-themes/themes.ts
+++ src/app/scenario/terminal-themes/themes.ts
@@ -3,6 +3,7 @@ import Solarized_Light from './Solarized_Light';
 import Solarized_Dark from './Solarized_Dark';
 import Solarized_Dark_Higher_Contrast from './Solarized_Dark_Higher_Contrast';
 import GitHub from './GitHub';
+import Dichromatic from './Dichromatic';
 
 //Themes taken from https://github.com/ysk2014/xterm-theme
 const availableThemes = [
@@ -10,7 +11,8 @@ const availableThemes = [
     {'theme': 'Solarized_Light', 'name': 'Solarized Light', 'styles': Solarized_Light},
     {'theme': 'Solarized_Dark', 'name': 'Solarized Dark', 'styles': Solarized_Dark},
     {'theme': 'Solarized_Dark_Higher_Contrast', 'name': 'Solarized Dark Higher Contrast', 'styles': Solarized_Dark_Higher_Contrast },
-    {'theme': 'GitHub', 'name': 'GitHub', 'styles': GitHub }
+    {'theme': 'GitHub', 'name': 'GitHub', 'styles': GitHub },
+    {'theme': 'Dichromatic', 'name': 'Dichromatic', 'styles': Dichromatic }
 ];
 
 export {availableThemes};
