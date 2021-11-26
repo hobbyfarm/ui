734cc2a5397ec2c0ef489316f46d1d3b3694a170
Raphael von der Grün <raphael.vondergruen@sva.de>
2021-11-26 16:35:13 +0100

    fix(unicode): add missing parameter types

--- src/app/unicode.ts
+++ src/app/unicode.ts
@@ -1,7 +1,7 @@
-export function atou(b64) {
+export function atou(b64: string) {
     return decodeURIComponent(escape(atob(b64)));
 }
 
-export function utoa(data) {
+export function utoa(data: string) {
     return btoa(unescape(encodeURIComponent(data)));
 } 
