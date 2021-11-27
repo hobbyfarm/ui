0df1cd4d6023be6aa1e665cebf1ac73be8ddcb59
Raphael von der Grün <raphael.vondergruen@sva.de>
2021-11-28 00:02:45 +0100

    cleaner markdown extension

--- src/app/hf-markdown/hf-markdown.component.ts
+++ src/app/hf-markdown/hf-markdown.component.ts
@@ -28,108 +28,114 @@ export class HfMarkdownComponent {
     public markdownService: MarkdownService
   ) {
     this.markdownService.renderer.code = (code: string, language: string) => {
-      // block text
-      if (language.length == 0) {
-        if (/~~~([\s\S]*?)~~~/.test(code)) {
-          let content: string = '';
-          const codeArray: string[] = code.split('~~~');
-          codeArray.forEach((codePart: string, index: number) => {
-            // First part inside a block outside nested blocks
-            if (index == 0) {
-              content += escape('\n' + codePart);
-
-              // This case occurs outside nested blocks
-            } else if (index % 2 == 0) {
-              content += escape(codePart).replace(/^\s/, '');
-
-              // This case occurs when an odd number of tildes appear within a fenced block
-              // and therefore not all of them can be resolved.
-            } else if (index % 2 != 0 && index == codeArray.length - 1) {
-              content += escape('~~~' + codePart);
-
-              // This case occurs inside nested blocks
-            } else if (codePart) {
-              content += this.markdownService.compile('~~~' + codePart + '~~~');
-            } else {
-              content += '~~~~~~';
-            }
-          });
-          return '<pre>' + content + '</pre>';
-        } else {
-          // code block is empty or only contains white spaces
-          if (!code.trim()) {
-            return '<pre></pre>';
-          }
-          // Prevent leading blank lines from being removed on non-empty code blocks
-          else if (/^\n/.test(code)) {
-            return '<pre>' + '\n' + escape(code) + '</pre>';
-          } else {
-            return '<pre>' + escape(code) + '</pre>';
-          }
-        }
-      }
-
-      // determine what kind of special injection we need to do
-      if (language.split(':')[0] == 'ctr') {
-        // generate a new ID
-        var id = ctrService.generateId();
-        let maxCount = Number.POSITIVE_INFINITY;
-        ctrService.setCode(id, code);
-        // split the language (ctr:target)
-        ctrService.setTarget(id, language.split(':')[1]);
-
-        if (
-          language.split(':').length > 2 &&
-          !isNaN(Number(language.split(':')[2]))
-        ) {
-          maxCount = Number(language.split(':')[2]);
-        }
-
-        ctrService.setCount(id, maxCount);
-
-        return '<ctr ctrid="' + id + '"></ctr>';
-      } else if (language.split(':')[0] == 'vminfo') {
-        var config = new VMInfoConfig();
-        config.id = this.vmInfoService.generateId();
-        config.name = language.split(':')[1];
-        config.info = language.split(':')[2];
-        config.ss = this.sessionId;
-        config.mode = language.split(':')[3];
-        config.code = code;
-        this.vmInfoService.setConfig(config);
-
-        return `<vminfo id="${config.id}"></vminfo>`;
-      } else if (language.split(':')[0] == 'hidden') {
-        return (
-          '<details>' +
-          '<summary>' +
-          language.split(':')[1] +
-          '</summary>' +
-          this.markdownService.compile(code) +
-          '</details>'
-        );
-      } else if (language.split(':')[0] == 'glossary') {
-        return (
-          "<div class='glossary'>" +
-          language.split(':')[1] +
-          "<span class='glossary-content'>" +
-          this.markdownService.compile(code) +
-          '</span></div>'
-        );
+      const [tag, ...args] = language.split(':');
+      if (tag in this.taggedCodeRenderers) {
+        const renderer = this.taggedCodeRenderers[tag];
+        return renderer.call(this, code, ...args);
+      } else if (language.length > 0) {
+        return this.renderHighlightedCode(code, language);
+      } else if (/~~~([\s\S]*?)~~~/.test(code)) {
+        return this.renderNestedPlainCode(code);
       } else {
-        // highlighted code
-        return (
-          "<pre class='language-" +
-          language +
-          "'>" +
-          "<code class='language-" +
-          language +
-          "'>" +
-          escape(code) +
-          '</code>' +
-          '</pre>'
-        );
+        return this.renderSimplePlainCode(code);
       }
     };
+  }
+
+  private readonly taggedCodeRenderers: {
+    [tag: string]: (this: HfMarkdownComponent, code: string, ...args: string[]) => string;
+  } = {
+    ctr(code: string, target: string, countStr: string) {
+      var id = this.ctrService.generateId();
+      this.ctrService.setCode(id, code);
+      this.ctrService.setTarget(id, target);
+
+      let count = Number(countStr);
+      if (isNaN(count)) {
+        count = Number.POSITIVE_INFINITY;
+      }
+      this.ctrService.setCount(id, count);
+
+      return `<ctr ctrid="${id}"></ctr>`;
+    },
+
+    vminfo(code: string, name: string, info: string, mode: string) {
+      const config = new VMInfoConfig();
+      config.id = this.vmInfoService.generateId();
+      config.name = name;
+      config.info = info;
+      config.ss = this.sessionId;
+      config.mode = mode;
+      config.code = code;
+      this.vmInfoService.setConfig(config);
+
+      return `<vminfo id="${config.id}"></vminfo>`;
+    },
+
+    hidden(code: string, summary: string) {
+      return `
+        <details>
+          <summary>${summary}</summary>
+          ${this.markdownService.compile(code)}
+        </details>
+      `;
+    },
+
+    glossary(code: string, term: string) {
+      return `
+        <div class="glossary">
+          ${term}
+          <span class='glossary-content'>
+            ${this.markdownService.compile(code)}
+          </span>
+        </div>
+      `;
+    },
+  };
+
+  private renderHighlightedCode(code: string, language: string) {
+    const classAttr = `class="language-${language}"`;
+    const codeNode = `<code ${classAttr}>${escape(code)}</code>`;
+    return `<pre ${classAttr}>${codeNode}</pre>`;
+  }
+
+  private renderNestedPlainCode(code: string) {
+    let content: string = '';
+    const codeArray: string[] = code.split('~~~');
+    codeArray.forEach((codePart: string, index: number) => {
+      // First part inside a block outside nested blocks
+      if (index == 0) {
+        content += escape('\n' + codePart);
+
+        // This case occurs outside nested blocks
+      } else if (index % 2 == 0) {
+        content += escape(codePart).replace(/^\s/, '');
+
+        // This case occurs when an odd number of tildes appear within a fenced block
+        // and therefore not all of them can be resolved.
+      } else if (index % 2 != 0 && index == codeArray.length - 1) {
+        content += escape('~~~' + codePart);
+
+        // This case occurs inside nested blocks
+      } else if (codePart) {
+        content += this.markdownService.compile('~~~' + codePart + '~~~');
+      } else {
+        content += '~~~~~~';
+      }
+    });
+    return `<pre>${content}</pre>`;
+  }
+
+  private renderSimplePlainCode(code: string) {
+    // code block is empty or only contains white spaces
+    if (!code.trim()) {
+      return '<pre></pre>';
+    }
+    // Prevent leading blank lines from being removed on non-empty code blocks
+    else if (/^\n/.test(code)) {
+      return '<pre>' + '\n' + escape(code) + '</pre>';
+    } else {
+      return '<pre>' + escape(code) + '</pre>';
+    }
   }
 }
