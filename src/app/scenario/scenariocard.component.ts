cc1741883aef77032edbf292730b8ba43ca9fc5f
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2021-12-15 15:49:22 +0100

    Get user progress status

--- src/app/scenario/scenariocard.component.ts
+++ src/app/scenario/scenariocard.component.ts
@@ -4,23 +4,37 @@ import { Scenario } from '../scenario/Scenario';
 import { ServerResponse } from '../ServerResponse';
 import { environment } from 'src/environments/environment';
 import { atou } from '../unicode';
+import { ProgressService } from "../services/progress.service";
+import { Progress } from "../Progress";
+import { Router } from "@angular/router";
 
 @Component({
     templateUrl: 'scenariocard.component.html',
-    selector: 'scenario-card'
+    selector: 'scenario-card',
+    styleUrls: ['./scenariocard.component.scss']
 })
 export class ScenarioCard implements OnInit {
     @Input()
     public scenarioid: string = "";
     @Input()
     public courseid: string = "";
+    @Input()
+    public activeSession: boolean = false;
+    @Input()
+    public progress: Progress;
+
+    public hasProgress: boolean = false;
+    public terminated: boolean = false;
+
     @Output()
     scenarioModal = new EventEmitter();
 
     public scenario: Scenario = new Scenario();
 
     constructor(
-        private http: HttpClient
+        private http: HttpClient,
+        private router: Router,
+        private progressService: ProgressService
     ) {
     }
 
@@ -31,7 +45,62 @@ export class ScenarioCard implements OnInit {
                 this.scenario = JSON.parse(atou(s.content));
             },
         )
+        if(!this.progress){
+            this.getProgressData();
+        }else{
+            this.hasProgress = true;
+        }
     }
+
+    continue(){
+        this.router.navigateByUrl("/app/session/" + this.progress.session + "/steps/" + Math.max(this.progress.current_step-1,0));
+    }
+
+    terminate(){
+        this.terminated = true;
+        this.http.put(environment.server + "/session/" + this.progress.session + "/finished", {})
+        .subscribe()
+    }
+
+    getProgressData(){
+        this.progressService.watch().subscribe(
+            (p: Progress[]) => 
+            {
+                p = p.filter(prog => prog.scenario == this.scenarioid && prog.finished == true)
+                this.progress = p.pop();
+                if(this.progress){
+                    this.hasProgress = true;
+                }
+                p.forEach(progress => {
+                    if(progress.max_step >= this.progress.max_step){
+                        this.progress = progress;
+                       
+                    }
+                })
+            }
+        )
+    }
+
+    getProgress(){
+        if(this.terminated){
+            return 100;
+        }
+        if(this.progress.total_step == 0){
+            return 0;
+        }
+        return Math.floor((this.progress.current_step / this.progress.total_step ) * 100);
+    }
+
+    public getProgressColorClass(){
+        if(this.terminated){
+            return "status-finished"
+        }
+
+        if(this.progress.max_step == this.progress.total_step){
+            return "status-success";
+        }
+        return "status-running";
+      }
 
     navScenario() {
        this.scenarioModal.emit({s:this.scenarioid,c:this.courseid});
--- src/app/services/progress.service.ts
+++ src/app/services/progress.service.ts
@@ -1,13 +1,59 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { ServerResponse } from '../ServerResponse';
+import { map, tap } from 'rxjs/operators';
+import { atou } from '../unicode';
+import { Progress, ProgressStep } from '../Progress';
+import { BehaviorSubject, of } from 'rxjs';
 
 @Injectable()
 export class ProgressService {
+    private cachedProgressList: Progress[] = []
+    private bh: BehaviorSubject<Progress[]> = new BehaviorSubject(this.cachedProgressList);
+    private fetchedList = false;
 
     constructor(
         private http: HttpClient
     ){}
+
+    public watch(){
+        return this.bh.asObservable();
+    }
+
+    public list(force: boolean = false) {
+        if (!force && this.fetchedList)  {
+            return of(this.cachedProgressList);
+        } else{
+            return this.http.get(environment.server + "/progress/list")
+            .pipe(
+                map((s: ServerResponse) => {
+                    return JSON.parse(atou(s.content));
+                }),
+                map((pList: Progress[]) => {
+                    pList.forEach((p: Progress) => {
+                        p.last_update = new Date(p.last_update);
+                        p.started = new Date(p.started);
+                        p.finished = JSON.parse(String(p.finished))
+                        p.steps.forEach((s: ProgressStep) => {
+                            s.timestamp = new Date(s.timestamp);
+                        })
+                    });
+                    return pList;
+                }),
+                tap((p: Progress[]) => {
+                    this.set(p);
+                    }
+                  )
+            )
+        }
+  }
+
+    public set(list: Progress[]){
+        this.cachedProgressList = list;
+        this.fetchedList = true;
+        this.bh.next(list);
+    }
 
     public update(session: string, step: number) {
         let body = new HttpParams()
