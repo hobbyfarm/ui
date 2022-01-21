c22c40b96096045f627c8aaa742b279fa62016cc
Raphael von der Grün <raphael.vondergruen@sva.de>
2022-01-21 17:08:39 +0100

    refactor(Scenario): add printable attribute to Scenario

--- src/app/scenario/PrintableScenario.ts
+++
@@ -1,9 +0,0 @@
-export class PrintableScenario {
-    id: string;
-    name: string;
-    description: string;
-    stepcount: number;
-    virtualmachines: Map<string, string>[];
-    pauseable: boolean;
-    printable: boolean;
-}
