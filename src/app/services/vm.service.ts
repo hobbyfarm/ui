03af174b71e40c6cab461c2f543b34fc2baff37f
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2023-02-10 13:25:01 +0100

    run prettierg

--- src/app/services/vm.service.ts
+++ src/app/services/vm.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ResourceClient, GargantuaClientFactory } from './gargantua.service';
 import { VM } from '../VM';
-import { catchError, } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -17,12 +17,12 @@ export class VMService extends ResourceClient<VM> {
 
     return super.get(id);
   }
-  
+
   getWebinterfaces(id: string) {
-    return this.garg.get("/getwebinterfaces/"+id).pipe(
+    return this.garg.get('/getwebinterfaces/' + id).pipe(
       catchError((e: HttpErrorResponse) => {
-      return throwError(e.error);
-    }),
-    )
+        return throwError(e.error);
+      }),
+    );
   }
 }
