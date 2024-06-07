b3435a9232f09f7b9eabed428cf97163fdd5ee03
PhPrinz <48761034+PhPrinz@users.noreply.github.com>
2024-06-07 12:54:24 +0200

    Fix home for courses with less scenarios, refactor utf8 decoding (#207)
    * fix course overview if courses only have one scenario
    
    * use and improve existing atou function instead of new function
    
    ---------
    
    Co-authored-by: Philip Prinz <Philip.Prinz@sva.de>

--- src/app/unicode.ts
+++ src/app/unicode.ts
@@ -1,5 +1,12 @@
 export function atou(b64: string) {
-  return decodeURIComponent(escape(atob(b64)));
+  const text = atob(b64);
+  const length = text.length;
+  const bytes = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    bytes[i] = text.charCodeAt(i);
+  }
+  const decoder = new TextDecoder(); // default is utf-8
+  return decoder.decode(bytes);
 }
 
 export function utoa(data: string) {
