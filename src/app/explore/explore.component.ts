a0e7a25190dc966dfcaef3798051ef19eb318c94
pprinzSVA <philip.prinz@sva.de>
2025-04-11 10:20:46 +0200

    prevent access to explore courses after reload

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnDestroy, OnInit } from '@angular/core';
 import { Course } from '../course/course';
 import { CourseService } from '../services/course.service';
-import { ContextService } from '../services/context.service';
+import { Context, ContextService } from '../services/context.service';
+import { ReplaySubject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.scss'],
 })
-export class ExploreComponent implements OnInit {
+export class ExploreComponent implements OnInit, DoCheck, OnDestroy {
   input = '';
   catalog: Course[] = [];
+  private ctx: Context;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
   constructor(
     private courseService: CourseService,
@@ -18,10 +21,24 @@ export class ExploreComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.contextService.set('');
-    this.courseService.getCatalog().subscribe((courseList: Course[]) => {
-      this.catalog = courseList;
-    });
+    this.courseService
+      .getCatalog()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((courseList: Course[]) => {
+        this.catalog = courseList;
+      });
+    this.contextService
+      .watch()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((ctx) => {
+        this.ctx = ctx;
+      });
+  }
+
+  ngDoCheck() {
+    if (this.ctx?.accessCode !== '') {
+      this.contextService.set('');
+    }
   }
 
   matchesSearch(courseName: string) {
@@ -29,5 +46,10 @@ export class ExploreComponent implements OnInit {
     if (atob(courseName).toLowerCase().includes(this.input.toLowerCase()))
       return true;
     return false;
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
   }
 }
